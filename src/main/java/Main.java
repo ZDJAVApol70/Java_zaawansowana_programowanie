3f9b97d0db620e1ea0b6a332ae4caa02daffc6b3
Krzysztof <wrycza.k@interia.pl>
2021-03-07 13:50:06 +0100

    userService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import pl.sda.model.*;
+import pl.sda.service.UserService;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,5 +23,23 @@ public class Main {
         System.out.println(user1.toString());
         BigDecimal sumOfAmounts = user1.sumAmount();
         System.out.println(sumOfAmounts);
+        User user= new User("KRzysztof",address1,accounts);
+        User user2= new User("Tomek",address1,accounts);
+        User user3= new User("Szymon",address1,accounts);
+        User user4= new User("Mateusz",address1,accounts);
+        user.setOpeningDate(LocalDate.of(2020,12,12));
+        user2.setOpeningDate(LocalDate.of(2020,10,12));
+        user3.setOpeningDate(LocalDate.of(2020,8,12));
+        user4.setOpeningDate(LocalDate.of(2020,4,12));
+
+        List<User> users= new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        UserService userService = new UserService();
+        List<User> byDate = userService.findByDate(users, LocalDate.of(2020, 7, 12), LocalDate.of(2020, 12, 1));
+        System.out.println(byDate);
+
     }
 }
