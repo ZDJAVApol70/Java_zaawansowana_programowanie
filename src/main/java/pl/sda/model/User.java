fb157b0d79729c8d0cebff1df23f275fc81d663b
19Pawel97 <19pawel9772@gmail.com>
2021-03-07 11:50:30 +0100

    Przeliczanie

--- src/main/java/pl/sda/model/User.java
+++ src/main/java/pl/sda/model/User.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.function.Supplier;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +15,16 @@ public class User extends Account{
     private String name;
     private Address address;
     private List<BankAccount> accounts;
+
+    public BigDecimal sumAmount() {
+        BigDecimal sum = accounts.stream()
+                .map(bankAccount -> {
+                    Amount amount = bankAccount.getAmount();
+                    BigDecimal amountInPln = amount.getValue().multiply(BigDecimal.valueOf(amount.getCurrency().getExchangeRate()));
+                    return amountInPln;
+                })
+                .reduce((bigDecimal, bigDecimal2) -> bigDecimal.add(bigDecimal2))
+                .orElse(BigDecimal.ZERO);
+       return sum;
+    }
 }
